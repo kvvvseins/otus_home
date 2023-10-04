54c0497f9818fa87de7429bfeb5340513b7920ad
Костин Владимир Валерьевич <vladimir.kostin@vitech.team>
2023-10-04 23:01:14 +0300

    Реализация списка и доп тесты на них

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,74 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("removeTest", func(t *testing.T) {
+		l := NewList()
+
+		// пробуем удалить из пустого списка
+		l.Remove(&ListItem{Value: 11})
+		require.Equal(t, 0, l.Len())
+
+		l.PushFront(11)
+
+		// пробуем удалить из списка одного элемента
+		l.Remove(&ListItem{Value: 11})
+		require.Equal(t, 0, l.Len())
+
+		l.PushFront(10)
+		l.PushBack(20)
+		l.PushBack(35)
+		l.PushBack(30)
+
+		// пробуем удалить первый элемент списка
+		l.Remove(l.Front())
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 30, l.Back().Value)
+
+		l.PushFront(10)
+
+		// пробуем удалить последний элемент списка
+		l.Remove(l.Back())
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 35, l.Back().Value)
+	})
+
+	t.Run("MoveToFront test", func(t *testing.T) {
+		l := NewList()
+
+		item0 := l.PushFront(0)
+		item1 := l.PushFront(1)
+		item2 := l.PushFront(2)
+		item3 := l.PushFront(3)
+		item4 := l.PushFront(4)
+		item5 := l.PushFront(5)
+
+		l.MoveToFront(item2)
+
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, (*ListItem)(nil), l.Front().Prev)
+
+		require.Equal(t, 0, l.Back().Value)
+		require.Equal(t, (*ListItem)(nil), l.Back().Next)
+
+		require.Equal(t, item5.Value, item2.Next.Value)
+		require.Equal(t, (*ListItem)(nil), item2.Prev)
+
+		require.Equal(t, item4.Value, item5.Next.Value)
+		require.Equal(t, item2.Value, item5.Prev.Value)
+
+		require.Equal(t, item3.Value, item4.Next.Value)
+		require.Equal(t, item5.Value, item4.Prev.Value)
+
+		require.Equal(t, item1.Value, item3.Next.Value)
+		require.Equal(t, item4.Value, item3.Prev.Value)
+
+		require.Equal(t, item0.Value, item1.Next.Value)
+		require.Equal(t, item3.Value, item1.Prev.Value)
+
+		require.Equal(t, (*ListItem)(nil), item0.Next)
+		require.Equal(t, item1.Value, item0.Prev.Value)
+	})
 }
