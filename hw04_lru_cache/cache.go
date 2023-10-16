93db00701291f2a88289ca8a5a61934a9294207c
Костин Владимир Валерьевич <vladimir.kostin@vitech.team>
2023-10-16 20:51:28 +0300

    Реализация кеша и доп тесты на них

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,11 +11,10 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mu       *sync.Mutex
 }
 
 func NewCache(capacity int) Cache {
@@ -21,5 +22,58 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		mu:       &sync.Mutex{},
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if val, ok := c.items[key]; ok {
+		val.Value = value
+		c.queue.MoveToFront(val)
+
+		return true
+	}
+
+	if c.queue.Len() >= c.capacity {
+		lastItem := c.queue.Back()
+
+		c.queue.Remove(lastItem)
+
+		for k, v := range c.items {
+			if v == lastItem {
+				delete(c.items, k)
+
+				break
+			}
+		}
+	}
+
+	c.queue.PushFront(value)
+	c.items[key] = c.queue.Front()
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if value, ok := c.items[key]; ok {
+		c.queue.MoveToFront(value)
+
+		return value.Value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
